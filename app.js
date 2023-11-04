207224cd98fdeadd94a40a68394fc69ed1575ad8
Kaushik Varanasi <kaushik@Kaushiks-MacBook-Pro.local>
2023-11-04 12:20:30 +0530

    added routes for twitter

--- app.js
+++ app.js
@@ -1,16 +1,50 @@
-// Copyright 2021 Twitter, Inc.
-// SPDX-License-Identifier: Apache-2.0
+// jshint esversion: 6
 
-import { Client } from "twitter-api-sdk";
-// import * as dotenv from "dotenv";
+const express = require("express");
+var bodyParser = require("body-parser");
+const axios = require("axios")
+const addOAuthInterceptor = require("axios-oauth-1.0a")
 
-// dotenv.config();
-const client = new Client(process.env.BEARER_TOKEN);
-console.log(process.env.BEARER_TOKEN);
+const app = express();
+const port =3000
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-async function main() {
-    const tweet = await client.tweets.findTweetById("20");
-    console.log(tweet.data.text);
-}
-  
-main();
+
+app.post("/tweet", function (req, res) {
+    // Create a client whose requests will be signed
+    const client = axios.create();
+    // Specify the OAuth options
+    const options = {
+        algorithm: 'HMAC-SHA1',
+        key: 'yQfuGgSP12lEz6FlFxJWC7hYF',
+        secret: 'DZoWs6kTrJZ37h4cWKZXnTT4WPk0Ry718hRhcUZHTBhsLqG7lE',
+        token: '1054546747-79Bb6CQEwrQFt05eKpHwpubLKDCWvqFaLSfGn3A',
+        tokenSecret: 'jrLrp9Vd8jJdthxPlThXVTXzdaqjpbeDqeev9KAkt931X',
+    };
+
+    const data = {
+        "text": "Hello status sir!"
+    };
+
+    console.log("addOAuthInterceptor is not a function: ", addOAuthInterceptor)
+    // Add interceptor that signs requests
+    addOAuthInterceptor.default(client, options);
+
+    client.post("https://api.twitter.com/2/tweets", 
+    data)
+    .then(response => {
+        console.log("resp: ", response);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+    res.send(`<h2> the result is : Tweeted </h2>` );
+});
+
+app.listen(port, () => console.log(`calculator listening on port {port}!`))
+
+module.exports = app
